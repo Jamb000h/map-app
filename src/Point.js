7147e88f94e98d65167feebde51c39d42d737e8b
Jamb000h <jamb000h@gmail.com>
2017-10-05 23:39:10 +0300

    Added opening & closing times - next up: refactoring to just GMaps API instead of using a react library

--- src/Point.js
+++ src/Point.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 
+// MAP API KEY AIzaSyBivuF4JYT0fg7cFCa-Ork7fvMiMVq6ujU 
+
 class Point extends Component {
   handleClick = (event) => {
     event.preventDefault();
     event.stopPropagation();
     event.target.nextSibling.style.display = event.target.nextSibling.style.display === 'block' ? 'none' : 'block';
   }
+
+  renderDates = (value, index) => {
+    if(value.time === '00:00' && this.props.data.closingHours[index].time === '00:00') {
+      return <td>24h</td>
+    } 
+    return <td>{value.time} - {this.props.data.closingHours[index].time}</td>
+  }
+
   render () {
+
+    const weekday = new Date().getDay()
+
     return (
-      <div className="map-marker" onClick={this.handleClick} data-id={this.props.key}>
-        <img src={require('./mapPointMarker.png')} className="mapPointMarker" />
+      <div className="map-marker" data-id={this.props.key}>
+        <img src={require('./mapPointMarker.png')} className="mapPointMarker" onClick={this.handleClick} />
         <div className="map-marker-popup" data-id={this.props.key}>
           <h3>{this.props.data.title}</h3>
-          <p>{this.props.data.description}</p>
+          <p>
+            {this.props.data.address}
+          </p>
+          <br />
+          <table>
+            <thead>
+              <td>Mon</td>
+              <td>Tue</td>
+              <td>Wed</td>
+              <td>Thu</td>
+              <td>Fri</td>
+              <td>Sat</td>
+              <td>Sun</td>
+            </thead>
+            <tr>
+              {this.props.data.openingHours.map(this.renderDates)}
+            </tr>
+          </table>
         </div>
       </div>
     )
